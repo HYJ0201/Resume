6fd80fc6928f4516fa06a356796c3a65f91532c3
胡朋飞 <572229761@qq.com>
2020-09-16 23:39:11 +0800

    添加实体

---
+++ Resume/Resume.Web/Models/DataConntection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Web.Models
+{
+    public class DataConntection: DbContext
+    {
+    }
+}
---
+++ Resume/Resume.Web/Models/UserInfoEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resume.Web.Models
+{
+    public class UserInfoEntity
+    {
+        public int ID { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 文件
+        /// </summary>
+        public IFormFile File { get; set; }
+
+    }
+}
